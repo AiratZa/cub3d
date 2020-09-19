c638f9aefee6b9153bdbb4d177f61bf9efe76d6f
Great Drake <gdrake@er-c2.kzn.21-school.ru>
2020-09-19 08:31:36 +0300

    add .cub extension check

--- parser/main.c
+++ parser/main.c
@@ -6,11 +6,23 @@
 /*   By: gdrake <gdrake@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/17 18:29:57 by gdrake            #+#    #+#             */
-/*   Updated: 2020/09/17 21:48:03 by gdrake           ###   ########.fr       */
+/*   Updated: 2020/09/19 08:30:20 by gdrake           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map_parser.h"
+
+void	name_check(char *map_name, t_vars *vars)
+{
+	int len;
+
+	len = ft_strlen(map_name);
+	if (len < 5)
+		ft_exit(EXIT_FAILURE, vars, 1, 0);
+	if ((map_name[len - 1] != 'b') || (map_name[len - 2] != 'u') ||
+		(map_name[len - 3] != 'c') || (map_name[len - 4] != '.'))
+		ft_exit(EXIT_FAILURE, vars, 1, 0);
+}
 
 void	map_params_parser(char *map_name, t_vars *vars)
 {
@@ -18,6 +30,7 @@ void	map_params_parser(char *map_name, t_vars *vars)
 	int	only_map_left;
 
 	only_map_left = 0;
+	name_check(map_name, vars);
 	initial_params(vars);
 	if ((fd = open(map_name, O_RDONLY)) < 0)
 		ft_exit(EXIT_FAILURE, vars, 1, 0);
